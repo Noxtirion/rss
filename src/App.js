65cb8d148ef9194ce6a71abe249913570cb2fbf3
Darek <dardman88@gmail.com>
2020-03-03 11:08:23 +0100

    Add component FeedPage

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from "react";
 import "./scss/style.scss";
 import TopBar from "./components/TopBar";
 import Form from "./components/Form";
+import { Switch, Route } from "react-router-dom";
+import FeedPage from "./components/FeedPage";
 
 function App() {
+   // const feed = localStorage.getItem("feed");
+
    return (
       <div>
          <TopBar />
-         <div className="appWrapper">
-            <Form />
-         </div>
+         <Switch>
+            <Route exact path="/">
+               <div className="appWrapper">
+                  <Form />
+               </div>
+            </Route>
+            {/* <Route exact path={`/${feed[0].name}`}>
+               <FeedPage />
+            </Route> */}
+         </Switch>
       </div>
    );
 }
--- src/components/CollectionPage.js
+++ src/components/CollectionPage.js
@@ -4,12 +4,13 @@ import { Link } from "react-router-dom";
 
 function CollectionPage(props) {
    console.log(props);
+   // localStorage.setItem("feed", { props });
 
    return (
       <div className="collectionPage">
          <h3>{props.name} </h3>
          <h3>{props.url}</h3>
-         <Link to="/feed">
+         <Link to={`/${props.name}`}>
             <Button text="Open" />
          </Link>
          <Button text="Delete" />
