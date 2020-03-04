f6da227b439058b1befb94e324c688c423a45f02
Darek <dardman88@gmail.com>
2020-03-04 13:36:33 +0100

    Add useForm package

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,10 +3,12 @@ import Button from "./Button";
 import CollectionPage from "./CollectionPage";
 import { observer } from "mobx-react";
 import { toJS } from "mobx";
+import { useForm } from "react-hook-form";
 
 function Form({ store }) {
    const [inputData, useInputData] = useState({ name: "", url: "" });
    const [feedData, useFeedData] = useState([]);
+   const { handleSubmit, register, errors } = useForm();
 
    const HandleChange = e => {
       const { name, value } = e.target;
@@ -19,7 +21,7 @@ function Form({ store }) {
    };
 
    const HandleSubmit = e => {
-      e.preventDefault();
+      // e.preventDefault();
       useFeedData(prevFeedData => [...prevFeedData, inputData]);
 
       // store.setFeedStore(feedData);
@@ -36,23 +38,39 @@ function Form({ store }) {
 
    return (
       <>
-         <form className="form" onSubmit={HandleSubmit}>
+         <form className="form" onSubmit={handleSubmit(HandleSubmit)}>
             <label>Name:</label>
             <input
                type="text"
                value={inputData.name}
                name="name"
+               ref={register({
+                  required: true,
+                  maxLength: {
+                     value: 15,
+                     message: "Max 15 characters..."
+                  }
+               })}
                placeholder="Add name..."
                onChange={HandleChange}
             />
+            <h3>{errors.name && errors.name.message}</h3>
             <label>URL:</label>
             <input
-               type="text"
+               type="url"
                value={inputData.url}
                name="url"
+               ref={register({
+                  required: true,
+                  pattern: {
+                     value: /(https?:\/\/)?([\w-])+\.{1}([a-zA-Z]{2,63})([/\w-]*)*\/?\??([^#\n\r]*)?#?([^\n\r]*)/,
+                     message: "Invalid url address"
+                  }
+               })}
                placeholder="Add URL..."
                onChange={HandleChange}
             />
+            <h3>{errors.url && errors.url.message}</h3>
             <Button text="Add" />
          </form>
          {addCollection}
