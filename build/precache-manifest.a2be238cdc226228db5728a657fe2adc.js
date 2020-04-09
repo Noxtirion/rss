ced8fb2ff96188ac4966cdd7e3b87a4ff9c7f12c
Darek <dardman88@gmail.com>
2020-04-09 18:31:34 +0200

    Remove build from .gitignore

---
+++ build/precache-manifest.a2be238cdc226228db5728a657fe2adc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b6f73d04f60fdbedde18e6e92416ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71de298bdf043772fcb0",
+    "url": "/static/css/main.5a79d4f9.chunk.css"
+  },
+  {
+    "revision": "b5f516ef3b2cace0d3c7",
+    "url": "/static/js/2.1fedb9c6.chunk.js"
+  },
+  {
+    "revision": "2ae196ad4a4d2e277a7fb23db1b3d6ec",
+    "url": "/static/js/2.1fedb9c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71de298bdf043772fcb0",
+    "url": "/static/js/main.7555c050.chunk.js"
+  },
+  {
+    "revision": "cb9cf53b198af1cfc6cb",
+    "url": "/static/js/runtime-main.f5964dcf.js"
+  },
+  {
+    "revision": "74050c577eec9146c454453d817a1876",
+    "url": "/static/media/arrowup.74050c57.svg"
+  },
+  {
+    "revision": "4631764624191712b975356750ac15c5",
+    "url": "/static/media/enter.46317646.svg"
+  },
+  {
+    "revision": "e7e3d09eae5daf4c852ba5e8be92824b",
+    "url": "/static/media/home.e7e3d09e.svg"
+  },
+  {
+    "revision": "9c95b6a2b06414f8f71b36bc0268b69a",
+    "url": "/static/media/newspaper.9c95b6a2.svg"
+  }
+]);
